8fb94a3c0dc292ed02eb7e7b31014f36b9877f9b
Ary Sugiarto <arysugiarto10@gmail.com>
2020-09-27 19:35:14 +0700

    Hello world + Type Data Number +Type Data Character

---
+++ src/Character.kt
@@ -0,0 +1,14 @@
+fun main() {
+//    petiknya satu bro
+    var a:Char = 'A'
+    var r:Char ='R'
+    var y:Char= 'Y'
+
+    var angka:Char ='1'
+
+    print(a)
+    print(r)
+    println(y)
+
+    println(angka)
+}
